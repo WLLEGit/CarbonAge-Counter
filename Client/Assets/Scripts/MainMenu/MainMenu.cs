6e2ad3a0262fbbca575bae0106734ebae946991d
WLLE <342970955@qq.com>
2021-05-09 19:31:42 +0800

    补全缺少的脚本

--- Client/Assets/Scripts/MainMenu/MainMenu.cs
+++ Client/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public InputField InputField;
+    public Dropdown DropdownSelecter;
+    public RectTransform Buttons;
+    public RectTransform InputAndSelect;
+
     public void PlayGame(){
-        SceneManager.LoadScene(1);
+        StartCoroutine(TranslateButtonsSmoothly(100));
     }
     public void QuitGame(){
         Application.Quit();
     }
     public void Settings(){
         SceneManager.LoadScene(2);
+    }
+    public void OnSelectAndInputDone()
+    {
+        ClientNetwork.ClientNetworkInstance.ConnectServer(InputField.text, 1357);
+        ClientNetwork.ClientNetworkInstance.EnterGame(DropdownSelecter.options[DropdownSelecter.value].text);
+        SceneManager.LoadScene("Battle");
+        InputAndSelect.gameObject.SetActive(false);
+    }
+    private IEnumerator TranslateButtonsSmoothly(int dis)
+    {
+        for (int i = 0; i < dis; ++i)
+        {
+            Buttons.transform.Translate(Vector3.left, Space.World);
+            yield return null;
+        }
+        InputAndSelect.gameObject.SetActive(true);
     }
 }
