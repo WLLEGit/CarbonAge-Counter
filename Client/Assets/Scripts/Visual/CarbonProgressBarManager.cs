4903062ac285cf82af2bb322132cd503c918491d
WLLE <342970955@qq.com>
2021-05-23 20:36:54 +0800

    调整了碳槽

--- Client/Assets/Scripts/Visual/CarbonProgressBarManager.cs
+++ Client/Assets/Scripts/Visual/CarbonProgressBarManager.cs
@@ -7,20 +7,33 @@ public class CarbonProgressBarManager : MonoBehaviour
 {
     public Image ProgressBar;
     public Image PlayerImage;
+
+    public Sprite[] CarbonBarSprites;
     // Start is called before the first frame update
     void Start()
     {
-        ProgressBar.fillAmount = 0;
+        ProgressBar.fillAmount = (float)0.5;
+        //StartCoroutine(ChangeCarbonBarSprite());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     public void SetCarbonPercent(double amount)
     {
         Debug.Log("SetPercent Called, " + amount.ToString());
         ProgressBar.fillAmount = (float)amount;
+    }
+    private IEnumerator ChangeCarbonBarSprite()
+    {
+        while(true)
+        {
+            foreach(var sprite in CarbonBarSprites)
+            {
+                ProgressBar.sprite = sprite;
+                yield return new WaitForSeconds((float)0.1);
+            }
+        }
     }
 }
